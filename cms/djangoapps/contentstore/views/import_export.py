28b0f849f5a4d225d6ade027f0fb5b6eb5e271ce
Adolfo R. Brandes <adolfo@hastexo.com>
2017-06-13 18:56:34 -0300

    Fix async course export
    Fix asynchronous course export so that it works with Swift as a storage
    backend.

--- cms/djangoapps/contentstore/views/import_export.py
+++ cms/djangoapps/contentstore/views/import_export.py
@@ -370,18 +370,7 @@ def export_status_handler(request, course_key_string):
     elif task_status.state == UserTaskStatus.SUCCEEDED:
         status = 3
         artifact = UserTaskArtifact.objects.get(status=task_status, name='Output')
-        if hasattr(artifact.file.storage, 'bucket'):
-            filename = os.path.basename(artifact.file.name).encode('utf-8')
-            disposition = 'attachment; filename="{}"'.format(filename)
-            output_url = artifact.file.storage.url(artifact.file.name, response_headers={
-                'response-content-disposition': disposition,
-                'response-content-encoding': 'application/octet-stream',
-                'response-content-type': 'application/x-tgz'
-            })
-        else:
-            # local file, serve from the authorization wrapper view
-            output_url = reverse_course_url('export_output_handler', course_key)
-
+        output_url = artifact.file.storage.url(artifact.file.name)
     elif task_status.state in (UserTaskStatus.FAILED, UserTaskStatus.CANCELED):
         status = max(-(task_status.completed_steps + 1), -2)
         errors = UserTaskArtifact.objects.filter(status=task_status, name='Error')
