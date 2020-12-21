c65bfe7ed40b802d4240fc3177400e7824b972e4
Maari Tamm <maari.tamm@citynetwork.eu>
2020-12-21 16:30:32 +0200

    Use "Authorization: Bearer <token>" header for Badgr API
    Using the Authorization header as "Token <token>" is no longer supported.
    To be able to use the Badgr API we'll need to update it to: "Bearer <token>".

--- lms/djangoapps/badges/backends/badgr.py
+++ lms/djangoapps/badges/backends/badgr.py
@@ -162,7 +162,7 @@ class BadgrBackend(BadgeBackend):
         """
         Headers to send along with the request-- used for authentication.
         """
-        return {'Authorization': u'Token {}'.format(settings.BADGR_API_TOKEN)}
+        return {'Authorization': u'Bearer {}'.format(settings.BADGR_API_TOKEN)}
 
     def _ensure_badge_created(self, badge_class):
         """
